26e31a18f27ff82cca90224ae7e3feb12214694b
Nate1306 <netanelrouach@gmail.com>
2025-10-19 08:37:08 +0300

    Update natanel.py

--- natanel.py
+++ natanel.py
@@ -1,22 +1,28 @@
-def guest_name():
-    return input("enter a guest")
-def less_than(lst,N):
-    return len(lst) < N
-def check_in(nm,lst):
-    return nm in lst
+def get_guest_name():
+    return input("Enter a guest: ")
+
+def is_list_shorter_than(guest_list, max_guests):
+    return len(guest_list) < max_guests
+
+def is_already_invited(name, guest_list):
+    return name in guest_list
 
 
-guest_list=[]
-for x in range(3):
-    guest_list.append(guest_name())
+guest_list = []
+for count in range(3):
+    guest_list.append(get_guest_name())
+
 print(guest_list)
-while less_than(guest_list,12):
-    n=guest_name()
-    if not(check_in(n,guest_list)):
-        if (len(guest_list)+1)%2==0:
-            guest_list.insert(0,n)
+
+while is_list_shorter_than(guest_list, 12):
+    new_guest = get_guest_name()
+    if not is_already_invited(new_guest, guest_list):
+        if (len(guest_list) + 1) % 2 == 0:
+            guest_list.insert(0, new_guest)
         else:
-            guest_list.append(n)
+            guest_list.append(new_guest)
     else:
-        print(n,"is already invited")
+        print(new_guest, "is already invited")
+
 print(guest_list)
+
